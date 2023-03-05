68a8a3117649580bf07e16d8f25038fe7db79ec8
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-03-05 10:40:05 -0500

    initial setup of server and files. server works

---
+++ server/routes/math.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+let mathList = []
+
+//stays on bottom
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+const mathRouter = require('./routes/math.router.js');
+
+
+app.use(express.json());
+
+app.use('/math', mathRouter);
+
+app.use(express.static('server/public'));
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+})
